97913e0d1d7f4bd198635ad7fe7581b607b093a3
Rushikesh Dhumal <rushikeshdhumal0500@gmail.com>
2025-04-04 11:43:30 -0400

    Updated tailwind.config.js
    To add dark mode and extend font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
+  darkMode: 'class',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif']
+      }
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
